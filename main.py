88c7f80adda072abcd13e27462d6a58175b324c5
Federico Desmoulin <ricodemontmartre@Federicos-MacBook-Pro.local>
2025-06-26 17:39:08 +0200

    Keep your classes small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from email.message import EmailMessage
-from smtplib import SMTP_SSL
+
+from email_tools.service import EmailService
 
 SMTP_SERVER = "smtp.gmail.com"
 PORT = 465
@@ -9,29 +9,14 @@ PASSWORD = "password"
 
 
 @dataclass
-class Person:
-    name: str
+class Stats:
     age: int
-    address_line_1: str
-    address_line_2: str
-    city: str
-    country: str
-    postal_code: str
-    email: str
-    phone_number: str
     gender: str
     height: float
     weight: float
     blood_type: str
     eye_color: str
     hair_color: str
-
-    def split_name(self) -> tuple[str, str]:
-        first_name, last_name = self.name.split(" ")
-        return first_name, last_name
-
-    def get_full_address(self) -> str:
-        return f"{self.address_line_1}, {self.address_line_2}, {self.city}, {self.country}, {self.postal_code}"
 
     def get_bmi(self) -> float:
         return self.weight / (self.height**2)
@@ -46,35 +31,43 @@ class Person:
         else:
             return "Obese"
 
+@dataclass
+class Address:
+    address_line_1: str
+    address_line_2: str
+    city: str
+    country: str
+    postal_code: str
+
+    def get_full_address(self) -> str:
+        return f"{self.address_line_1}, {self.address_line_2}, {self.city}, {self.country}, {self.postal_code}"
+
+@dataclass
+class Person:
+    name: str
+    email: str
+    phone_number: str
+    address: Address
+    stats: Stats
+
+    def split_name(self) -> tuple[str, str]:
+        first_name, last_name = self.name.split(" ")
+        return first_name, last_name
+
     def update_email(self, email: str) -> None:
         self.email = email
-        # send email to the new address
-        msg = EmailMessage()
-        msg.set_content(
-            "Your email has been updated. If this was not you, you have a problem."
+        email_service = EmailService(SMTP_SERVER, PORT, EMAIL, PASSWORD)
+        email_service.send_message(
+            self.email,
+            "Your email has been updated.",
+            "Your email has been updated. If this was not you, you have a problem.",
         )
-        msg["Subject"] = "Your email has been updated."
-        msg["To"] = self.email
-
-        with SMTP_SSL(SMTP_SERVER, PORT) as server:
-            # server.login(EMAIL, PASSWORD)
-            # server.send_message(msg, EMAIL)
-            pass
-        print("Email sent successfully!")
 
 
 def main() -> None:
-    # create a person
-    person = Person(
-        name="John Doe",
+    # create stats
+    stats = Stats(
         age=30,
-        address_line_1="123 Main St",
-        address_line_2="Apt 1",
-        city="New York",
-        country="USA",
-        postal_code="12345",
-        email="johndoe@gmail.com",
-        phone_number="123-456-7890",
         gender="Male",
         height=1.8,
         weight=80,
@@ -83,10 +76,29 @@ def main() -> None:
         hair_color="Black",
     )
 
+
+    # create an address
+    address = Address(
+        address_line_1="123 Main St",
+        address_line_2="Apt 1",
+        city="New York",
+        country="USA",
+        postal_code="12345",
+    )
+
+    # create a person
+    person = Person(
+        name="John Doe",
+        email="johndoe@gmail.com",
+        phone_number="123-456-7890",
+        address=address,
+        stats=stats,
+    )
+
     # compute the BMI
-    bmi = person.get_bmi()
+    bmi = person.stats.get_bmi()
     print(f"Your BMI is {bmi:.2f}")
-    print(f"Your BMI category is {person.get_bmi_category()}")
+    print(f"Your BMI category is {person.stats.get_bmi_category()}")
 
     # update the email address
     person.update_email("johndoe@outlook.com")
